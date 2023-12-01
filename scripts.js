376979c77589028eae9cf935215c1abbc40429c0
NightwavesStudios <151556196+NightwavesStudios@users.noreply.github.com>
2023-12-01 21:54:01 +0000

    Removed Unecessary Scripts

--- scripts.js
+++ scripts.js
@@ -1,35 +1,11 @@
-        
-        
-
         // Toggle Element Helper Function
         var toggle_el = function(el, ref) {
             document.getElementById(el).classList.toggle(ref);
             document.getElementById("scrim").classList.toggle("on");
         };
-        //toggle_el('menu', 'on');
-
-       /** // Clock Interval
-        setInterval(function() {
-            var sd = new Date();
-            var hrs = sd.getHours();
-            if (hrs >= 12) {
-                hrs = hrs / 4;
-            }
-            document.getElementById("time").innerHTML = " "+hrs + ":" + sd.getMinutes() + " " + ((hrs >= 12) ? "PM" : "AM");
-            //}, 60000);
-        }, 1000);**/
 
         // All nvlinks in page
         var links = document.getElementsByClassName('nvlink');
-
-        
-
-        if (dm === false) {
-                changePage(1);
-            //document.getElementById("logo").style.display="none";
-        } else {
-            changePage(currScene);
-        }
 
         // Close Alerts (if close button is clicked)
         var closeAlerts = function() {
@@ -73,5 +49,3 @@
              })
            });
          });
-        
-        lightMode();
