d4fd612a8abf41f7f35f2dead6e0e54ac25d38f2
NightwavesStudios <151556196+NightwavesStudios@users.noreply.github.com>
2023-12-01 21:58:20 +0000

    Added Forms.js

--- scripts.js
+++ scripts.js
@@ -14,38 +14,4 @@
             }
         }
         
-        window.addEventListener("load", function() {
-           const form = document.getElementById('my-form');
-           form.addEventListener("submit", function(e) {
-             console.log("Form is Requesting Transfer");
-             e.preventDefault();
-             const data = new FormData(form);
-             const action = e.target.action;
-             fetch(action, {
-               method: 'POST',
-               body: data,
-             })
-             .then(() => {
-               console.log("Form Sucessfully Transfered");
-               changePage("2");
-               $("body").css("overflow-y", "scroll"); 
-             })
-           });
-         });
-          window.addEventListener("load", function() {
-           const submitArticle = document.getElementById('submitArticle');
-           submitArticle.addEventListener("submit", function(e) {
-             console.log("Form is Requesting Transfer");
-             e.preventDefault();
-             const submitData = new FormData(submitArticle);
-             const submitAction = e.target.action;
-             fetch(submitAction, {
-               method: 'POST',
-               body: submitData,
-             })
-             .then(() => {
-               console.log("Form Sucessfully Transfered");
-               window.location.href = "recipes.html";
-             })
-           });
-         });
+        
