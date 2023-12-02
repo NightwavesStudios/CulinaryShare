7eba7aafea52e601456666bd21cea84058ec66c4
OrigamiKing79 <152706314+OrigamiKing79@users.noreply.github.com>
2023-12-02 14:18:51 +0000

    Updated games page

--- data/games.js
+++ data/games.js
@@ -1,54 +1,16 @@
 var games = [{
-    name: "Essentials of Cooking 1",
+    name: "King of Steak",
     link: "javascript:(openLesson('essentialsofcooking1'));",
     thumb: "about.jpg",
-    votes: "10/10",
+    votes: "Reaction Time",
 },
 {
-    name: "Essentials of Cooking 2",
+    name: "Carrot Clicker",
     link: "javascript:(openLesson('essentialsofcooking2'));",
     thumb: "about.jpg",
-    votes: "10/10",
+    votes: "Clicker",
 },
-{
-    name: "Essentials of Cooking 3",
-    link: "javascript:(openLesson('essentialsofcooking3'));",
-    thumb: "about.jpg",
-    votes: "10/10"
-},
-{
-    name: "Essentials of Cooking 4",
-    link: "javascript:(openLesson('essentialsofcooking4'));",
-    thumb: "about.jpg",
-    votes: "10/10"
-},
-{
-    name: "Baking 101",
-    link: "javascript:(openLesson('baking101'));",
-    thumb: "baking.jpeg",
-    votes: "9/10"
-},
-{
-  name: "How to Grow Your Own Food",
-  link: "javascript:(openLesson('howtogrowyourownfood'));",
-  thumb: "gardening.jpeg",
-  votes: "7/10"
-  
-},
-{
-  name: "Grilling 101",
-  link: "javascript:(openLesson('grilling101'));",
-  thumb: "grilling.jpg",
-  votes: "6/10"
-  
-},
-{
-  name: "Basics of Plating",
-  link: "javascript:(openLesson('basicsofplating'));",
-  thumb: "platingdish.jpg",
-  votes: "6/10"
-  
-},
+
 ];
 
 function runGames() {
