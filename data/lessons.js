2b765e1bf57a51b3e6a9b9267523cd661e83a5d9
NightwavesStudios <151556196+NightwavesStudios@users.noreply.github.com>
2023-12-09 14:47:20 +0000

    Updated Data

--- data/lessons.js
+++ data/lessons.js
@@ -5,51 +5,7 @@ var lessons = [{
                 thumb: "about.jpg",
                 votes: "10/10",
             },
-            {
-                name: "Essentials of Cooking 2",
-                link: "lessons/essentials-of-cooking-2",
-                thumb: "about.jpg",
-                votes: "10/10",
-            },
-            {
-                name: "Essentials of Cooking 3",
-                link: "lessons/essentials-of-cooking-3",
-                thumb: "about.jpg",
-                votes: "10/10"
-            },
-            {
-                name: "Essentials of Cooking 4",
-                link: "lessons/essential-sof-cooking-4",
-                thumb: "about.jpg",
-                votes: "10/10"
-            },
-            {
-                name: "Baking 101",
-                link: "lessons/baking-101",
-                thumb: "baking.jpeg",
-                votes: "9/10"
-            },
-            {
-              name: "How to Grow Your Own Food",
-              link: "lessons/how-to-grow-your-own-food",
-              thumb: "gardening.jpeg",
-              votes: "7/10"
-              
-            },
-            {
-              name: "Grilling 101",
-              link: "lessons/grilling-101",
-              thumb: "grilling.jpg",
-              votes: "6/10"
-              
-            },
-            {
-              name: "Basics of Plating",
-              link: "lessons/basics-of-plating",
-              thumb: "platingdish.jpg",
-              votes: "6/10"
-              
-            },
+          
         ];
 
         function runLessons() {
