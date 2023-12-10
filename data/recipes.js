a9cdb149198f721c1443a8ce7d176a34d7acc549
NightwavesStudios <151556196+NightwavesStudios@users.noreply.github.com>
2023-12-10 14:10:18 -0500

    Updated Image Data

--- data/recipes.js
+++ data/recipes.js
@@ -9,25 +9,25 @@ var recipes = [
             {
                 name: "Grilled Cheese",
                 link: "/recipes/grilledcheese.html",
-                thumb: "grilledcheese.jpg",
+                thumb: "/assets/images/GrilledCheese.jpg",
                 votes: "Zaxbys",
             },
             {
                 name: "No-Bake Brownies",
                 link: "/recipes/nobakebrownies.html",
-                thumb: "nobakebrownies.jpg",
+                thumb: "/assets/images/NoBakeOreoBrownies.jpg",
                 votes: "7/10",
             },
             {
                 name: "Cinamon Oats",
                 link: "/recipes/cinamonoats",
-                thumb: "cinamonoats.jpg",
+                thumb: "/assets/images/CinnamonOats.jpg",
                 votes: "6/10",
             },
             {
                 name: "Apple Butter",
                 link: "/recipes/applebutter",
-                thumb: "applebutter.jpg",
+                thumb: "/assets/images/AppleButter.jpg",
                 votes: "6/10",
             },
             
